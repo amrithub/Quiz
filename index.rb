8d22fd41e46c96a2d7da70fbb715c4fe56c68d91
amrit <amritsk1@yahoo.com>
2019-11-16 10:35:58 +1100

    Score comparision table added

--- index.rb
+++ index.rb
@@ -5,16 +5,6 @@ puts a.asciify('Quiz by Amrit Sagar Khanal').colorize(:blue)
 
 
 def Quiz(qBank)
-    # qBankO = []
-    # File.open("qb.txt").each do|line|
-    #     qBankO.append(line)
-    # end
-    #puts qBankO ddbab
-    # qBankO =  ["what is the capital of Nepal? \n a. Kathmandu    b.Delhi     c. Sydney       d. Pokhara", "who is the president of USA? \n a. V. Putin      b. Justin Limbu     c. Yamaha Gamaguchi     d.Donald Trump",
-    # "what is the cause of fear? \n a. pain      b. gain     c.  nothing     d. all of above"]
-    # #puts   " qBank Malladi: #{qBankO}"
-    
-    #print qBankO
     qbank = []
     qBankO = []
     File.open("qb.txt").each do|line|
@@ -27,10 +17,8 @@ def Quiz(qBank)
         i += 2
     end
     item = qBank[rand(qBank.length)]
-    
-    puts " I     AM                AMRIT"
     print item
-    puts "Doing Good"
+    
     a = gets.chomp
     if item == qBankO[0]
         if 
@@ -39,14 +27,7 @@ def Quiz(qBank)
         else
             return [0,item,'All of the above'] 
         end
-    
-    # puts "Lets seeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"   
-    # puts qBankO[1]
     elsif item == qBankO[1] 
-        # print item
-        # print qBankO[1]
-        # print a
-        
         if 
             a == 'd'
             return [1,item,'All of the above']
@@ -72,10 +53,6 @@ def Quiz(qBank)
         else
             return [0,item,'.rb extension '] 
         end
-    # else 
-    
-    #     return [0,item,a]
-
     end
     puts
 end
@@ -90,18 +67,6 @@ for j in 0..4
     qBank[j] = qbank[i] + qbank[i+1]
     i += 2
 end
-#qBankO = qBank
-print qBank
-#     #puts line
-# for i in 1..5
-#     puts array[-i]
-# end
-# 
-# qBank =  ["what is the capital of Nepal? \n a. Kathmandu    b.Delhi     c. Sydney       d. Pokhara", "who is the president of USA? \n a. V. Putin      b. Justin Limbu     c. Yamaha Gamaguchi     d.Donald Trump",
-# "what is the cause of fear? \n a. pain      b. gain     c.  nothing     d. all of above"]
-# puts
-#qBankO = qBank
-# " qBank halladi: #{qBankO}"
 score = 0
 
 #b = 5
@@ -113,14 +78,19 @@ score = 0
 
 puts "Please enter your name:".colorize(:red)
 name = gets.chomp
-puts "How many quwstions do you want to take?"
-m = gets.chomp.to_i
-print m
-while m > 10 or m < 1
-    
-    puts "You entered invalid input. Please enter an integer between 1 to 10"
+puts "Press p for practice and t for tournament"
+mode = gets.chomp
+if mode == 'p'
+    puts "How many questions do you want to take?"
     m = gets.chomp.to_i
+
+    while m > 10 or m < 1
     
+        puts "You entered invalid input. Please enter an integer between 1 to 10"
+        m = gets.chomp.to_i
+    end
+else
+    m = 5
 end
 # begin
 #     name = gets.chomp
@@ -134,7 +104,7 @@ end
 #k = m.class
 #puts k
 # if m > 10 or m == 0 or m.class != Integer
-puts "Invalid input"#. Please enter an integer between 1 to 10 only!!!!!"
+
 puts
 
 # end
@@ -144,8 +114,6 @@ answers = []
 for i in 1..m
     # print "qBankO : #{qBankO}"
      a = Quiz(qBank)
-     #print a
-     print "flag"
      score += a[0]
      qBank.delete(a[1])
      answers.append(a[2])
@@ -160,18 +128,19 @@ puts "Anyway, here are the correct answers:".colorize(:blue)
 puts "answers:"
 puts 
 puts answers
-puts "The recent score is below. Compare your perforamnce wwith other's"
 
-File.open("scoreboard.txt",'a') do |line|
+if m == 5 and mode == 't'
+    puts "The recent score is below. Compare your perforamnce wwith other's"
+    File.open("scoreboard.txt",'a') do |line|
     # puts "The score card looks as below"
  
-             line.puts "#{name}:  #{score}"
+             line.puts "#{name}:            #{score}"
 
+    end
+    File.readlines('scoreboard.txt').last(5).each do |line|
+        puts line
+    end
 end
-File.readlines('scoreboard.txt').last(5).each do |line|
-    puts line
-  end
-#end
 #a = Quiz()
 #print 'a'
 #item = qBank[rand(qBank.length)]
